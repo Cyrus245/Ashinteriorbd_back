7a556ee285f040e769697c7353ec2a3c659f5570
Cyrus245 <you@example.com>
2024-10-05 12:44:41 +0600

    mongo cluseter

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 //import required libraries and files
-const express = require('express');
-var cors = require('cors');
+const express = require("express");
+var cors = require("cors");
 const app = express();
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser')
-const dotenv = require('dotenv');
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
-
-dotenv.config({ path: 'backend/config/config.env' })
+dotenv.config({ path: "backend/config/config.env" });
 
 //implement middleware
 app.use(cors());
@@ -15,31 +14,30 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 //Import all routes
-const auth = require('./routers/auth');
-const permissionRoute = require('./routers/permissionRoute');
-const contentheaderRoute = require('./routers/contentHeaderRoute');
-const productRoute = require('./routers/productRoute');
-const projectRoute = require('./routers/projectRoute');
-const galleryRoute = require('./routers/galleryRoute');
-const teamRoute = require('./routers/teamRoute');
-const contactRoute = require('./routers/contactRoute');
+const auth = require("./routers/auth");
+const permissionRoute = require("./routers/permissionRoute");
+const contentheaderRoute = require("./routers/contentHeaderRoute");
+const productRoute = require("./routers/productRoute");
+const projectRoute = require("./routers/projectRoute");
+const galleryRoute = require("./routers/galleryRoute");
+const teamRoute = require("./routers/teamRoute");
+const contactRoute = require("./routers/contactRoute");
 
-
-
-
-
-//API Route Middleware 
-app.use('/api/accounts/v1/', auth); 
-app.use('/api/accounts/v1/', permissionRoute);
-app.use('/api/content/v1/', contentheaderRoute);
-app.use('/api/product/v1/', productRoute);
-app.use('/api/project/v1/', projectRoute);
-app.use('/api/gallery/v1/', galleryRoute);
-app.use('/api/team/v1/', teamRoute);
-app.use('/api/contact/v1/', contactRoute);
-
-
+//API Route Middleware
+app.use("/api/accounts/v1/", auth);
+app.use("/api/accounts/v1/", permissionRoute);
+app.use("/api/content/v1/", contentheaderRoute);
+app.use("/api/product/v1/", productRoute);
+app.use("/api/project/v1/", projectRoute);
+app.use("/api/gallery/v1/", galleryRoute);
+app.use("/api/team/v1/", teamRoute);
+app.use("/api/contact/v1/", contactRoute);
+// GoJsrUVeWZj61tVa
+app._router.stack.forEach(function (r) {
+  if (r.route && r.route.path) {
+    console.log(r.route.path);
+  }
+});
 
 module.exports = app;
--- routers/contactRoute.js
+++ routers/contactRoute.js
@@ -1,24 +1,20 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //Import funtionalities from permission Controller
 const {
-    addContact,
-    listContact,
-    updateContact,
-    deleteContact,
-    
-    
-   
-} = require('../controllers/contactController')
+  addContact,
+  listContact,
+  updateContact,
+  deleteContact,
+  getTest,
+} = require("../controllers/contactController");
 
+router.route("/contact/add").post(addContact);
+router.route("/contact/list").get(listContact);
+router.route("/contact/update/:id").put(updateContact);
+router.route("/contact/delete/:id").delete(deleteContact);
+router.route("/contact/test").get(getTest);
 
-
-router.route('/contact/add').post(addContact);
-router.route('/contact/list').get(listContact);
-router.route('/contact/update/:id').put(updateContact);
-router.route('/contact/delete/:id').delete(deleteContact);
-
-
+console.log(`i am in app.js`);
 module.exports = router;
