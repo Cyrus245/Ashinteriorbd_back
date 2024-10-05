a86f7d7869576a37a238017a457cb9d58ca4ec53
Cyrus245 <you@example.com>
2024-10-06 03:58:32 +0600

    mongo atlas configured

--- config/database.js
+++ config/database.js
@@ -1,18 +1,32 @@
 //Database configuration file
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URL, {
-        // user: process.env.DB_USER,
-        // pass: process.env.DB_PASSWORD,
-        // dbName: process.env.DB_NAME,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(con => {
-        console.log(`MongoDB Database connected with HOST: ${con.connection.host}`);
-    })
-}
+  //   mongoose
+  //     .connect(process.env.DB_CONNECTION_STRING, {
+  //       // user: process.env.DB_USER,
+  //       // pass: process.env.DB_PASSWORD,
+  //       // dbName: process.env.DB_NAME,
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //     })
+  //     .then((con) => {
+  //       console.log(
+  //         `MongoDB Database connected with HOST: ${con.connection.host}`
+  //       );
+  //     });
 
+  mongoose
+    .connect(
+      "mongodb+srv://ashinteriorbd:x4peVyxysycjAKaI@ashcluster.q8bj7.mongodb.net/?retryWrites=true&w=majority&appName=ashCluster&tls=true"
+    )
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    });
+};
 
-module.exports = connectDatabase; 
+module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -1,34 +1,38 @@
 //import required files and libraries
-const app = require('./app')
-const connectDatabase = require('./config/database')
-const dotenv = require('dotenv');
+const app = require("./app");
+const connectDatabase = require("./config/database");
+const dotenv = require("dotenv");
 
 //handling uncaught exception
-process.on('uncaughtException', err => {
-    console.log(`Error: ${err.stack}`);
-    console.log(`Shutting Down Due to uncaught exception`);
-    process.exit(1);
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.stack}`);
+  console.log(`Shutting Down Due to uncaught exception`);
+  process.exit(1);
 });
 
 // Only load dotenv in development mode
-if (process.env.NODE_ENV !== 'production') {
-    dotenv.config({ path: 'config/config.env' });
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: "config/config.env" });
 }
 
-//connect to database 
+//connect to database
 connectDatabase();
 
-const PORT = process.env.PORT || 4000;  // Make sure to use the Render provided PORT
+const PORT = process.env.PORT || 4000; // Make sure to use the Render provided PORT
 
 const server = app.listen(PORT, () => {
-    console.log(`Backend Server started on PORT: ${PORT} in ${process.env.NODE_ENV || 'development'}`);
+  console.log(
+    `Backend Server started on PORT: ${PORT} in ${
+      process.env.NODE_ENV || "development"
+    }`
+  );
 });
 
 //handling unhandled promise rejection
-process.on('unhandledRejection', err => {
-    console.log(`Error: ${err.stack}`);
-    console.log(`Shutting Down the server due to unhandled promise rejection`);
-    server.close(() => {
-        process.exit(1);
-    });
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.stack}`);
+  console.log(`Shutting Down the server due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
 });
